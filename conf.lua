e8b1a4da07e04c03b7f7afdf6e6538d998e97f8f
panzertype <rellowy@gmail.com>
2024-07-17 21:15:30 +0300

    Disabled unused modules

--- conf.lua
+++ conf.lua
@@ -5,4 +5,13 @@ function love.conf(t)
     t.window.height = WINDOW_HEIGHT
     t.window.title = WINDOW_TITLE
     t.window.resizable = true
+
+    t.modules.data = false
+    t.modules.math = false
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.modules.system = false
+    t.modules.thread = false
+    t.modules.touch = false
+    t.modules.video = false
 end
